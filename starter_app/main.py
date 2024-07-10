502c974ec7f3acda85d946915d5c4a0bd4a6d5ab
Abhay-Sandhu <abhay.sandhu07@gmail.com>
2024-07-10 19:31:21 +0530

    Starter app| boilerplate

---
+++ starter_app/main.py
@@ -0,0 +1,14 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
+
+# app settings
+app = QApplication([])
+main = QWidget()
+main.setWindowTitle("Starter App")
+main.resize(600,500)
+
+# Create app objects
+
+# execute app
+main.show()
+app.exec_()
