9ce87adf1a91ee84de90a52c8646db76ad14f02c
Abhay-Sandhu <abhay.sandhu07@gmail.com>
2024-07-11 19:40:27 +0530

    Calculator app Key_Press

--- Calculator/app.py
+++ Calculator/app.py
@@ -10,12 +10,14 @@ class Calculator(QWidget):
 
         # Calculator objects
         self.display = QLineEdit()
+        self.display.setReadOnly(True)
         self.buttons = [
                 '1','2','3','/',
                 '4','5','6','*',
                 '7','8','9','-',
                 '0','.','=','+'
                 ]
+        self.button_dict = {}
         self.clear = QPushButton("Clear")
         self.delete = QPushButton("<")
         
@@ -58,18 +60,31 @@ class Calculator(QWidget):
 
     def add_buttons_to_grid(self):
         row, col = 0, 0
-        button_dict = {}
         for btn_text in self.buttons:
-            button_dict[btn_text] = QPushButton(btn_text)
-            button_dict[btn_text].clicked.connect(self.button_clicked)
-            self.grid.addWidget(button_dict[btn_text], row, col)
+            self.button_dict[btn_text] = QPushButton(btn_text)
+            self.button_dict[btn_text].clicked.connect(self.button_clicked)
+            self.grid.addWidget(self.button_dict[btn_text], row, col)
             col += 1
             if col > 3:
                 row +=1
                 col = 0
     
-    def keyPressEvent(self, a0: QKeyEvent):
-        return super().keyPressEvent(a0)
+    def keyPressEvent(self, event):
+        key = event.key()
+        num_pad_keys = [Qt.Key_1, Qt.Key_2, Qt.Key_3, Qt.Key_4, Qt.Key_5, Qt.Key_6, Qt.Key_7, # type: ignore
+                   Qt.Key_8, Qt.Key_9, Qt.Key_0, Qt.Key_Period, Qt.Key_Plus, Qt.Key_Minus, # type: ignore
+                   Qt.Key_Asterisk, Qt.Key_Slash] # type: ignore
+        result_keys = [Qt.Key_Equal, Qt.Key_Return, Qt.Key_Enter] # type: ignore
+        if key in num_pad_keys:
+            self.button_dict[chr(key)].click()
+        elif key in result_keys:
+            self.button_dict['='].click() 
+        elif key in [Qt.Key_Backspace, Qt.Key_Delete]: # type: ignore
+            self.delete.click()
+        elif key == Qt.Key_C: # type: ignore
+            self.clear.click()
+        else:
+            super().keyPressEvent(event)
 
 
 
