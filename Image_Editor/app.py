f6020d54a617d030a0fb2210b887cae48c8745f7
Abhay-Sandhu <abhay.sandhu07@gmail.com>
2024-07-13 13:23:02 +0530

    Image_Editor refactor

--- Image_Editor/app.py
+++ Image_Editor/app.py
@@ -1,8 +1,8 @@
+from email.mime import image
 from PIL import Image, ImageFilter, ImageEnhance
 import os
-from os import listdir
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont, QKeyEvent, QPixmap, QImage
+from PyQt5.QtGui import QFont, QKeyEvent, QPixmap
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QListWidget, \
                             QComboBox, QVBoxLayout, QHBoxLayout, QVBoxLayout, QFileDialog
 
@@ -17,19 +17,20 @@ class Image_Editor(QWidget):
         self.select_folder = QPushButton("Select Folder")
         self.image_list = QListWidget()
         self.filter_box = QComboBox()
-        self.buttons = ["Left", "Right", "Mirror", "Sharpen", "B/W", "Color Saturation", "Contrast"]
+        self.buttons = ["Left", "Right", "Mirror", "Sharpen", "Blur", "Edge Enhance" ,"B/W", "Color Saturation", 
+                        "Contrast", "Emboss", "Smoothen", "Contour", "Detail"]
         self.button_dict = {}
+        
         # App Layout
         self.master_layout = QHBoxLayout()
         self.control_column = QVBoxLayout()
         self.image_column = QVBoxLayout()
-
         self.control_column.addWidget(self.select_folder)
         self.control_column.addWidget(self.image_list)
         self.control_column.addWidget(self.filter_box)
         self.filter_box.addItem("Original")
         self.add_buttons_to_column_and_filter_box()
-        self.image_column.addWidget(self.image_label, alignment=Qt.AlignCenter) 
+        self.image_column.addWidget(self.image_label, alignment=Qt.AlignCenter)
         self.master_layout.addLayout(self.control_column, 20)
         self.master_layout.addLayout(self.image_column, 80)
         self.setLayout(self.master_layout)
@@ -45,20 +46,15 @@ class Image_Editor(QWidget):
         # Event handlers
         self.select_folder.clicked.connect(self.select_folder_clicked)
         self.image_list.currentRowChanged.connect(self.image_list_changed)
-        #self.filter_box.currentTextChanged.
-        self.button_dict['Left'].clicked.connect(self.left_rotate)
-        self.button_dict['Right'].clicked.connect(self.right_rotate)
-        self.button_dict['Mirror'].clicked.connect(self.mirror_image)
-        self.button_dict['Sharpen'].clicked.connect(self.sharpen_image)
-        self.button_dict['B/W'].clicked.connect(self.black_and_white)
-        self.button_dict['Color Saturation'].clicked.connect(self.color_saturation)
-        self.button_dict['Contrast'].clicked.connect(self.contrast_image)
-        
+        self.filter_box.currentTextChanged.connect(self.apply_filter)
+       
+                         
     def add_buttons_to_column_and_filter_box(self):
 
         for btn_text in self.buttons:
             self.button_dict[btn_text] = QPushButton(btn_text)
             #self.button_dict[btn_text].setStyleSheet("QPushButton{font: 18pt Comic Sans MS; padding: 10px}")
+            self.button_dict[btn_text].clicked.connect(self.apply_filter)
             self.control_column.addWidget(self.button_dict[btn_text])
             self.filter_box.addItem(btn_text)
 
@@ -67,7 +63,7 @@ class Image_Editor(QWidget):
         self.working_directory = QFileDialog.getExistingDirectory()
         extensions = ['.png', '.jpg', '.jpeg', '.svg', '.bmp']
         self.image_list.clear()
-        filtered_images = [file for file in listdir(self.working_directory) if file.endswith(tuple(extensions))]
+        filtered_images = [file for file in os.listdir(self.working_directory) if file.endswith(tuple(extensions))]
         self.image_list.addItems(filtered_images)
 
     def image_list_changed(self):
@@ -98,59 +94,47 @@ class Image_Editor(QWidget):
     
 
     # FILTERS
-    def original_image(self):
+
+    def load_filter(self, filter_name):
+        if filter_name == 'Original':
+            self.pic = self.original_pic
+        else:
+            filter_map = {"Left":            lambda image : image.transpose(Image.Transpose.ROTATE_270),
+                          "Right":           lambda image : image.transpose(Image.Transpose.ROTATE_90),
+                          "Mirror":          lambda image : image.transpose(Image.Transpose.FLIP_LEFT_RIGHT),
+                          "Sharpen":         lambda image : image.filter(ImageFilter.SHARPEN),
+                          "Blur":            lambda image : image.filter(ImageFilter.BLUR),
+                          "Edge Enhance":    lambda image : image.filter(ImageFilter.EDGE_ENHANCE),
+                          "B/W":             lambda image : image.convert('L'),
+                          "Color Saturation":lambda image : ImageEnhance.Color(image).enhance(1.2),
+                          "Contrast":        lambda image : ImageEnhance.Contrast(image).enhance(1.2),
+                          "Emboss":          lambda image : image.filter(ImageFilter.EMBOSS),
+                          "Smoothen":        lambda image : image.filter(ImageFilter.SMOOTH), 
+                          "Contour":         lambda image : image.filter(ImageFilter.CONTOUR),
+                          "Detail":          lambda image : image.filter(ImageFilter.DETAIL)
+                        }
+            filter_function = filter_map.get(filter_name)
+            if filter_function:
+                self.pic = filter_function(self.pic)
+                self.save_image()
+                self.image_path = os.path.join(self.save_folder, self.image_file)
+                self.show_image(self.image_path)
+
         self.save_image()
-        self.image_path = os.path.join(self.working_directory, self.image_file)
+        self.image_path = os.path.join(self.save_folder, self.image_file)
         self.show_image(self.image_path)
 
-    def left_rotate(self):
+    def apply_filter(self):
         if self.pic is not None:
-            self.pic = self.pic.rotate(-90)
-            self.save_image()
-            self.image_path = os.path.join(self.save_folder, self.image_file)
-            self.show_image(self.image_path)
+            if str(type(self.sender())) == "<class 'PyQt5.QtWidgets.QComboBox'>" :
+                if self.filter_box.currentText() is not None:
+                    filter_name = self.filter_box.currentText()
+                    self.load_filter(filter_name)
+            elif str(type(self.sender())) == "<class 'PyQt5.QtWidgets.QPushButton'>":
+                print(str(self.sender().objectName()))
+                filter_name = self.sender().text()
+                self.load_filter(filter_name)
     
-    def right_rotate(self):
-        if self.pic is not None:
-            self.pic = self.pic.rotate(90)
-            self.save_image()
-            self.image_path = os.path.join(self.save_folder, self.image_file)
-            self.show_image(self.image_path)
-
-    def mirror_image(self):
-        if self.pic is not None:
-            self.pic = self.pic.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
-            self.save_image()
-            self.image_path = os.path.join(self.save_folder, self.image_file)
-            self.show_image(self.image_path)
-
-    def sharpen_image(self):
-        if self.pic is not None:
-            self.pic = ImageEnhance.Sharpness(self.pic).enhance(1.2)
-            self.save_image()
-            self.image_path = os.path.join(self.save_folder, self.image_file)
-            self.show_image(self.image_path)
-
-    def black_and_white(self):
-        if self.pic is not None:
-            self.pic = self.pic.convert('L')
-            self.save_image()
-            self.image_path = os.path.join(self.save_folder, self.image_file)
-            self.show_image(self.image_path)
-
-    def color_saturation(self):
-        if self.pic is not None:
-            self.pic = ImageEnhance.Color(self.pic).enhance(1.2)
-            self.save_image()
-            self.image_path = os.path.join(self.save_folder, self.image_file)
-            self.show_image(self.image_path)
-
-    def contrast_image(self):
-        if self.pic is not None:
-            self.pic = ImageEnhance.Contrast(self.pic).enhance(1.2)
-            self.save_image()
-            self.image_path = os.path.join(self.save_folder, self.image_file)
-            self.show_image(self.image_path)
 
 # Execute application
 if __name__ == '__main__':
